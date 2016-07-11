76bd05c7303255ec63e75021004b761a25fc6227
Bredyn Mccombs <bmccombs@cerium.cs.dixie.edu>
2016-07-11 14:05:16 -0600

    added require statments to gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
+var connect = require('gulp-connect'); // run local server (dev)
+var open = require('gulp-open'); // open our default browser
+var browserify = require('browserify'); // bundle javascript and lets us use the common js pattern in the front end
+var reactify = require('reactify'); // transpile our JSX to JS
+var source = require('vinyl-source-stream'); // use conventionaltext stream with gulp
+var concat = require('gulp-concat'); // concatenate our files
+
 
