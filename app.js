139c1abd0f7586630c31cb725080613a4b5452f3
Bredyn Mccombs <bmccombs@cerium.cs.dixie.edu>
2016-07-11 16:00:39 -0600

    set up gulp and server

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+
+
+app.use('*', function (req, res)
+{
+	res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(8000, function() {
+	console.log('Our app is using port 8000')
+});
+// console.log('Listening on port 8000');
