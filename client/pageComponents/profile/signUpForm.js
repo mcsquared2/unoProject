a95548cdae53581e1bb59df90c1409a6de71a664
Jorden Truman <jtruman@gold.cs.dixie.edu>
2016-07-14 14:28:04 -0600

    created signUpForm.js and worked on signUP.js

---
+++ client/pageComponents/profile/signUpForm.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var React = require ('react')
+
+var signUpForm = React.createClass({
+	render:function() {
+		return(
+			<form>
+				<TextInput
+					name = 'name'
+					placeholder = "Name"
+					value = {this.props.profile.name}
+					saveProfileState = {this.props.saveProfileState}
+					/>
+				<TextInput
+					name = 'email'
+					placeholder = "Email"
+					value = {this.props.profile.email}
+					saveProfileState = {this.props.saveProfileState}
+					/>
+				<TextInput
+					name = "username"
+					placeholder = "Username"
+					value = {this.props.profile.username}
+					saveProfileState = {this.props.saveProfileState}
+					/>
+				<TextInput
+					name = "password"
+					placeholder = "Password"
+					value = {this.props.profile.password}
+					saveProfileState = {this.props.saveProfileState}
+					/>
+				<input
+				className="card yellow" 
+				type="submit" 
+				value='Continue' 
+				/>
+			</form>
+
+
+		);
+	}
+});
+module.exports = signUpForm;
--- client/pageComponents/signUp.js
+++ client/pageComponents/signUp.js
@@ -1,17 +1,38 @@
 'use strict';
 
 var React = require('react'); 
+var signUpForm = require('../common/signUpForm')
 
 var signUp = React.createClass({
+	getInitialState:function() {
+		return{
+			profile: {
+				name: '',
+				email: '',
+				username: '',
+				password: '',
+
+			}
+		}
+	},
+
+	saveProfileState:function(event){
+		var field = event.target.name;
+		var value = event.target.value;
+
+	},
+
+
 	render: function () {
 		return (
 			<div>
-				<div>
-				</div>
-				<div>
-					<h1> Sign Up </h1>
-				</div>
+				<h1> Sign Up </h1>
 			</div>
+
+			<LoginForm
+				profile = {this.state.profile}
+				
+			/>
 
 		);
 	}
